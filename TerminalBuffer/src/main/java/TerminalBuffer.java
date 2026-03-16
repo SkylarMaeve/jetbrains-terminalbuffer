89b565f85f882fd35937c3a9eeb2544802b1bae9
Nikol Otáhalová <536358@mail.muni.cz>
2026-03-16 16:22:33 +0100

    feature: add Editing logic to TerminalBuffer

--- TerminalBuffer/src/main/java/TerminalBuffer.java
+++ TerminalBuffer/src/main/java/TerminalBuffer.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class TerminalBuffer {
     // Cursor
@@ -15,7 +17,7 @@ public class TerminalBuffer {
     private boolean styleFlagItalic = false;
     private boolean styleFlagUnderline = false;
 
-    private LinkedList<Cell[]> screen;
+    private Cell[][] screen;
     private LinkedList<Cell[]> scrollBack;
 
     public TerminalBuffer(int width, int height, int maxScrollback) {
@@ -31,6 +33,9 @@ public class TerminalBuffer {
         this.styleFlagBold = bold;
         this.styleFlagItalic = italic;
         this.styleFlagUnderline = underline;
+
+        this.screen = new Cell[height][width];
+        this.scrollBack = new LinkedList<>();
     }
 
     public void setForegroundColor(TerminalColor foregroundColor) {
@@ -81,6 +86,112 @@ public class TerminalBuffer {
 
     public void moveCursorDown(int numberOfColumns) {
         setCursorPosition(cursorColumn, cursorRow + numberOfColumns);
+    }
+
+    // Editing
+    public void writeText(String text) {
+        text.chars().forEach(this::writeCharacter);
+    }
+
+    public void insertText(String text) {
+        List<Cell> tailingCells = new ArrayList<>();
+        for (int row = cursorRow; row < height; row++) {
+            int startColumn = (row == cursorRow) ? cursorColumn : 0;
+            for (int column = startColumn; column < width; column++) {
+                tailingCells.add(screen[row][column]);
+            }
+        }
+
+        text.chars().forEach(this::writeCharacter);
+
+        int endRow = cursorRow;
+        int endColumn = cursorColumn;
+
+        tailingCells.forEach(cell -> writeCharacter(cell.character));
+
+        setCursorPosition(endRow, endColumn);
+    }
+
+    public void fillLine(char character) {
+        for (int column = 0; column < height; column++) {
+            Cell c = screen[cursorRow][cursorColumn];
+            c.character = character;
+
+            c.foregroundColor = foregroundColor;
+            c.backgroundColor = backgroundColor;
+
+            c.styleFlagBold = styleFlagBold;
+            c.styleFlagItalic = styleFlagItalic;
+            c.styleFlagUnderline = styleFlagUnderline;
+        }
+    }
+
+    public void insertEmptyLineAtBottom() {
+        pushToScrollback();
+        setCursorPosition(0, height - 1);
+    }
+
+    public void clearScreen() {
+        for (int row = cursorRow; row < height; row++) {
+            for (int column = cursorColumn; column < width; column++) {
+                Cell c = screen[row][column];
+                c.character = ' ';
+            }
+        }
+        setCursorPosition(0, 0);
+    }
+
+    public void clearAll() {
+        clearScreen();
+        scrollBack.clear();
+    }
+
+    // Providers
+    private void writeCharacter(int character) {
+        // EOL
+        if (character == '\n') {
+            cursorColumn = 0;
+            cursorRow++;
+            if (cursorRow >= height) {
+                pushToScrollback();
+                return;
+            }
+        }
+
+        Cell c = screen[cursorRow][cursorColumn];
+        c.character = character;
+
+        c.foregroundColor = foregroundColor;
+        c.backgroundColor = backgroundColor;
+
+        c.styleFlagBold = styleFlagBold;
+        c.styleFlagItalic = styleFlagItalic;
+        c.styleFlagUnderline = styleFlagUnderline;
+
+        cursorColumn++;
+        if (cursorColumn >= width) {
+            cursorColumn = 0;
+            cursorRow++;
+
+            if (cursorRow >= height) {
+                pushToScrollback();
+            }
+        }
+    }
+
+    private void pushToScrollback() {
+        if (maxScrollback > 0) {
+            scrollBack.addLast(screen[0]);
+            if (scrollBack.size() > maxScrollback) {
+                scrollBack.removeFirst();
+            }
+        }
+
+        for (int i = 1; i < height; i++) {
+            screen[i-1] = screen[i];
+        }
+        screen[height-1] = new Cell[width];
+        cursorRow = height - 1;
     }
 
 }
