eb5ef439e1214f0810e92f6386e8e335d53362e1
Nikol Otáhalová <536358@mail.muni.cz>
2026-03-16 13:39:16 +0100

    feature: add base TerminalBuffer attributes

--- TerminalBuffer/src/main/java/Cell.java
+++ TerminalBuffer/src/main/java/Cell.java
@@ -1,2 +1,11 @@
 public class Cell {
+    //Basics
+    public int character = ' ';
+    public TerminalColor foregroundColor = TerminalColor.DEFAULT;
+    public TerminalColor backgroundColor =  TerminalColor.DEFAULT;
+    public int width = 1;
+    //Style flags
+    public boolean styleFlagBold = false;
+    public boolean styleFlagItalic = false;
+    public boolean styleFlagUnderline = false;
 }
--- TerminalBuffer/src/main/java/TerminalBuffer.java
+++ TerminalBuffer/src/main/java/TerminalBuffer.java
@@ -1,2 +1,28 @@
+import java.util.LinkedList;
+
 public class TerminalBuffer {
+    // Cursor
+    private int cursorPositionX = 0;
+    private int cursorPositionY = 0;
+    // Attributes basics
+    private int width;
+    private int height;
+    private int maxScrollback;
+    private TerminalColor foregroundColor = TerminalColor.DEFAULT;
+    private TerminalColor backgroundColor =  TerminalColor.DEFAULT;
+    // Style flags
+    private boolean styleFlagBold = false;
+    private boolean styleFlagItalic = false;
+    private boolean styleFlagUnderline = false;
+
+    private LinkedList<Cell[]> screen;
+    private LinkedList<Cell[]> scrollBack;
+
+    public TerminalBuffer(int width, int height, int maxScrollback) {
+        this.width = width;
+        this.height = height;
+        this.maxScrollback = maxScrollback;
+    }
+
+
 }
